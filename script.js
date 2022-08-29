6789e75040273a9470a212627e1e9d013dd4856b
Salawu Abdulmalik <salawuabdulmalik90@gmail.com>
2022-08-29 10:59:52 -0700

    I built a basic calculator

---
+++ script.js
@@ -0,0 +1,12 @@
+function enterNo(myNumber){
+    result.value += myNumber 
+}
+function solve(){
+   result.value = eval(result.value)
+}
+function clearAll(){
+    result.value = "";
+}
+function myDelete(){
+    result.value = result.value.slice(0, -1)
+}
